025349c31f72225ae7a284646988ad1cabac6007
Joonion Bae <joonion@gmail.com>
2022-05-29 15:24:14 +0900

    파트 구분: 0, 1, 2, 3

--- Chap.04.마방진/24891.단어마방진/solve.py
+++ Chap.04.마방진/24891.단어마방진/solve.py
@@ -1,31 +1,42 @@
-
-
-def promising(D, S):
+def promising(i, m, D, S):
     if len(S) == 0:
         return True
     else:
-        return True
+        target, word = D[S[0]], D[i]
+        return word[0] == target[len(S)]
 
-def search(m, n, D, S, solution):
-    if m == 0 and promising(D, S):
-        candidate = "".join([D[s] for s in S])
-        solution[0] = min(solution[0], candidate)
+def check(D, S):
+    T = [D[s] for s in S]
+    for i in range(1, len(T)):
+        for j in range(i + 1, len(T[0])):
+            if T[i][j] != T[j][i]:
+                return False
+    return True
+    
+def search(m, n, D, S):
+    if m == 0:
+        return None if not check(D, S) else [D[s] for s in S]
     else:
         for i in range(n):
-            if i not in S and promising(D, S):
+            if i not in S and promising(i, m, D, S):
                 S.append(i)
-                search(m - 1, n, D, S, solution) 
+                solution = search(m - 1, n, D, S) 
+                if solution != None:
+                    return solution
                 S.pop()
-            
+        return None
+
 def solve(m, n, D):
-    solution = ["Z" * (m * m)]
-    search(m, n, D, [], solution)
-    if solution[0] == "Z" * (m * m):
+    D.sort()
+    solution = search(m, n, D, [])
+    if solution == None:
         print("NONE")
     else:
         for i in range(m):
-            print(solution[0][i*m : i*m + m])
-    
-L, N = map(int, input().split())
-D = [input() for _ in range(N)]
+            print(solution[i])
+
+import sys
+input = sys.stdin.readline
+L, N = map(int, input().strip().split())
+D = [input().strip() for _ in range(N)]
 solve(L, N, D)
